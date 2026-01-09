438e481c83b40a1e1284ace8ebb1ee3bcf498132
muaadahmed <muaad.abdi.ahmed@gmail.com>
2026-01-08 21:08:08 -0500

    Get lead values from DB to render them, second step; get only values from leads object and render on page

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ function render(leads) {
 }
 
 onValue(referenceInDB, function(snapshot) {
-    console.log(snapshot.val())
+   const snapshotValues = snapshot.val();
+   const leads = Object.values(snapshotValues);
+   render(leads)
 })
 
 deleteBtn.addEventListener("dblclick", function() {
